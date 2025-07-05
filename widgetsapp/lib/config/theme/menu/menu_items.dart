e88537f99e22499f47de8de3a69c467fa6add924
acensito <acenforos@gmail.com>
2025-07-06 00:04:18 +0200

    modificamos el estilo de appbar

---
+++ widgetsapp/lib/config/theme/menu/menu_items.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    icon: Icons.smart_button_outlined,
+    title: 'Botones',
+    subtitle: 'Varios botones en flutter',
+    link: '/buttons'),
+  MenuItem(
+    icon: Icons.credit_card,
+    title: 'Tarjetas',
+    subtitle: 'Un contenedor estilizado',
+    link: '/card')
+];
---
+++ widgetsapp/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:widgetsapp/config/theme/app_theme.dart';
+import 'package:widgetsapp/views/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).getTheme(),
+      home: HomeScreen()
+    );
+  }
+}
