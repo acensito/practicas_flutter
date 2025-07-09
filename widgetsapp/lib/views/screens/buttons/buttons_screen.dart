d798c63c7a01791b9b7d25ce2b7db2fa206a0717
acensito <acenforos@gmail.com>
2025-07-09 22:40:41 +0200

    difrentes tipos de botones

--- widgetsapp/lib/views/screens/buttons/buttons_screen.dart
+++ widgetsapp/lib/views/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -8,10 +9,49 @@ class ButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Buttons screen'),
+      appBar: AppBar(title: Text('Buttons screen')),
+      body: _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.pop();
+        },
+        child: Icon(Icons.arrow_back_ios_new_rounded),
       ),
-      body: Placeholder(),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Elevated Button')),
+            ElevatedButton(onPressed: null, child: Text('Elevated Disabled')),
+            ElevatedButton.icon(onPressed: () {}, icon: Icon(Icons.access_alarm_rounded), label: Text('Elevated Icon')),
+            FilledButton(onPressed: () {}, child: Text('Filled Button')),
+            FilledButton.icon(onPressed: () {}, icon: Icon(Icons.accessibility_new), label: Text('Filled Icon')),
+            OutlinedButton.icon(onPressed: () {}, icon: Icon(Icons.accessibility_new), label: Text('Outlined Icon')),
+            TextButton.icon(onPressed: () {}, icon: Icon(Icons.accessibility_new), label: Text('Text Icon')),
+            IconButton(onPressed: () {}, icon: Icon(Icons.accessibility_new)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.accessibility_new), tooltip: 'Icon Button'),
+            IconButton.filled(onPressed: () {}, icon: Icon(Icons.accessibility_new), style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(colors.secondary),
+            )), 
+
+          ],
+        ),
+      ),
     );
   }
 }
