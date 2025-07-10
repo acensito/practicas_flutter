8661181fcfd547293317996a8e702731dc851f00
acensito <acenforos@gmail.com>
2025-07-10 19:46:58 +0200

    screens creadas y barril actualizado

---
+++ widgetsapp/lib/views/screens/app_tutorial/app_tutorial.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTutorial extends StatelessWidget {
+
+  static const routeName = 'apptutorial_screen';
+
+  const AppTutorial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ widgetsapp/lib/views/screens/ui_controls/ui_controls.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UiControls extends StatelessWidget {
+
+  static const routeName = 'ui_controls_screen';
+
+  const UiControls({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
