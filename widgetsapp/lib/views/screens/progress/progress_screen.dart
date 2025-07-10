3bbae0f00afc7716fcf6bf1214e73abb771428f7
acensito <acenforos@gmail.com>
2025-07-10 20:26:59 +0200

    circular y linear personalizado

--- widgetsapp/lib/views/screens/progress/progress_screen.dart
+++ widgetsapp/lib/views/screens/progress/progress_screen.dart
@@ -26,9 +26,41 @@ class _ProgressView extends StatelessWidget {
           const Text('Progress Indicator'),
           CircularProgressIndicator(strokeWidth: 2),
           const SizedBox(height: 30),
-          const Text('Circular progress controlated'),
+          const Text('Circular y linear controlated'),
+          const _ControlledProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
 
-      ]),
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value*2) / 100; // Simulate a progress value from 0.0 to 1.0
+      }).takeWhile((value) => value <= 1.0),
+      builder: (context, snapshot) {
+
+        final progressValue = snapshot.data ?? 0.0;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(value: progressValue, strokeWidth: 2),
+              const SizedBox(width: 20),
+              Expanded(
+                child: LinearProgressIndicator(value: progressValue),
+              ),
+            ],
+          ),
+        );
+      }
     );
   }
 }
