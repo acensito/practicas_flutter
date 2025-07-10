a7b8389273c4a07ac68156eda65bb2d54c9fbda1
acensito <acenforos@gmail.com>
2025-07-10 21:10:50 +0200

    dialogs y licencias

--- widgetsapp/lib/views/screens/snackbar/snackbar_screen.dart
+++ widgetsapp/lib/views/screens/snackbar/snackbar_screen.dart
@@ -19,10 +19,68 @@ class SnackbarScreen extends StatelessWidget {
     );
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible:
+          false, // Evita que se cierre al tocar fuera del dialogo
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Dialogo de ejemplo'),
+          content: const Text(
+            'lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+            'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cerrar'),
+            ),
+            FilledButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Snackbars y Dialogos')),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  applicationName: 'Widgets App',
+                  applicationVersion: '1.0.0',
+                  applicationIcon: const Icon(Icons.widgets_outlined, size: 50),
+                  children: [
+                    const Text(
+                      'Esta es una aplicacion de ejemplo para mostrar widgets de Flutter.',
+                    ),
+                    const SizedBox(height: 10),
+                    const Text('Desarrollado por Felipon2T'),
+                  ],
+                );
+              },
+              child: Text('Licencias usadas'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: Text('Mostrar dialogo'),
+            ),
+          ],
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
         icon: const Icon(Icons.remove_red_eye_outlined),
