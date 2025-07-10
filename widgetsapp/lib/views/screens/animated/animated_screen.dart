6c3a0b3de45e925b59f8d13aedae8ab1d1057f7d
acensito <acenforos@gmail.com>
2025-07-10 21:49:53 +0200

    animated elastic

--- widgetsapp/lib/views/screens/animated/animated_screen.dart
+++ widgetsapp/lib/views/screens/animated/animated_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
 
   static const routeName = 'animated_screen';
 
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.indigo;
+  double _borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+
+    setState(() {
+      _width = random.nextInt(300)+50;
+      _height = random.nextInt(300)+50;
+      _color = Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      );
+      _borderRadius = random.nextInt(100)*20;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,18 +43,20 @@ class AnimatedScreen extends StatelessWidget {
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 300),
-          curve: Curves.easeOutCubic,
-          width: 200,
-          height: 100,
+          curve: Curves.elasticOut,
+          width: _width <= 0 ? 0 : _width,
+          height: _height <= 0 ? 0 : _height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20),
+            color: _color,
+            borderRadius: _borderRadius < 0
+                ? BorderRadius.circular(0)
+                : BorderRadius.circular(_borderRadius),
           ),
         )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Action to perform when the button is pressed
+          changeShape();
         },
         child: const Icon(Icons.play_arrow_rounded),
       ),
