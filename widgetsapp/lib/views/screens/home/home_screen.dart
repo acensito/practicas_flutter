59203c966995deba1094668b6961e283ceee3b10
acensito <acenforos@gmail.com>
2025-07-11 18:09:38 +0200

    NavigationDrawer implementation menu

--- widgetsapp/lib/views/screens/home/home_screen.dart
+++ widgetsapp/lib/views/screens/home/home_screen.dart
@@ -10,10 +10,14 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(title: Text('Flutter + Material 3')),
       body: _HomeView(),
-      drawer: SideMenu(),
+      drawer: SideMenu(scaffoldKey: scaffoldKey),
     );
   }
 }
--- widgetsapp/lib/views/widgets/drawers/side_menu.dart
+++ widgetsapp/lib/views/widgets/drawers/side_menu.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgetsapp/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
-  const SideMenu({super.key});
+
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const SideMenu({super.key, required this.scaffoldKey});
 
   @override
   State<SideMenu> createState() => _SideMenuState();
@@ -13,12 +18,44 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
-      selectedIndex: navIndex,
+      selectedIndex: null,
+      onDestinationSelected: (value) {
+        final menuItem = appMenuItems[value];
+        context.push(menuItem.link);
+        widget.scaffoldKey.currentState?.closeDrawer();
+      },
       children: [
-        NavigationDrawerDestination(icon: Icon(Icons.home), label: Text('Home')),
-        NavigationDrawerDestination(icon: Icon(Icons.settings), label: Text('Settings')),
-        NavigationDrawerDestination(icon: Icon(Icons.info), label: Text('About')),
+        Padding(padding: EdgeInsets.fromLTRB(28, hasNotch ? 10 : 0, 26, 10),
+          child: Text('Menu', style: Theme.of(context).textTheme.headlineSmall),
+        ),
+        //iteramos las opciones del menu
+        ...appMenuItems
+        .sublist(0,3)
+        .map((item) {
+          return NavigationDrawerDestination(
+            icon: Icon(item.icon),
+            label: Text(item.title),
+          );
+        }),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 28, vertical: 10),
+          child: Divider()
+        ),
+        Padding(padding: EdgeInsets.fromLTRB(28, 10, 26, 10),
+          child: Text('More options', style: Theme.of(context).textTheme.headlineSmall),
+        ),
+        ...appMenuItems
+        .sublist(3)
+        .map((item) {
+          return NavigationDrawerDestination(
+            icon: Icon(item.icon),
+            label: Text(item.title),
+          );
+        }),
       ],
     );
   }
