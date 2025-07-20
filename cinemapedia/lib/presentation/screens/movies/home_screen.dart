e4dddd4ce354291d839af4ac8983a944b5d2ebd9
acensito <acenforos@gmail.com>
2025-07-20 13:22:41 +0200

    home screen y router

---
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static final String routeName = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
